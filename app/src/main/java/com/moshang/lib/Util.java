926db9d572f054cbd454fd9a008fe6effcefaee0
Jerson <superjcn@126.com>
2021-07-05 16:09:02 +0800

    first commit

---
+++ app/src/main/java/com/moshang/lib/Util.java
@@ -0,0 +1,14 @@
+package com.moshang.lib;
+
+import android.util.Log;
+
+/**
+ * Created by:Jerson, on 2021/7/5.
+ * Describe:
+ **/
+public class Util {
+
+    public static void log(String s){
+        Log.e("debug",s);
+    }
+} 
